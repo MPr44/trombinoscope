0eae72636cf1ff94477c2379795d626d8d419111
Marius <marius.sergent@gmail.com>
2024-12-04 15:46:52 +0100

    Add exercice instructions

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,13 @@ async function init () {
 }
 
 init()
+
+/**
+ * Mise en application
+ */
+
+// Etape 1 : Intégrer les données sur la page HTML
+// 1) Utiliser les données dans l'objet "data" pour afficher une liste à puce avec le nom et le prénom de chaque salarié
+// 1.a) Modifier le fichier HTML pour avoir un <ul> dans le <main> et ajouter un ID au main
+// 1.b) Cibler/Récupérer l'élément HTML <ul> à partir du <main>
+// 1.c) A l'aide d'une boucle et d'un fragment, ajouter des <li> pour chacun des salarié contenu dans le tableau "data"
